09921bd3daa43183d83f21c8e87666528b2781e5
artemave <artemave@gmail.com>
2017-11-27 19:43:25 +0000

    Use relative path for OCI_* variables
    Because oracle client is built and linked in `/tmp/build/app` but then
    in runtime ends up in `/app`.

--- test/compile_test.sh
+++ test/compile_test.sh
@@ -4,10 +4,12 @@
 
 testCompile() {
   compile
+  assertCapturedSuccess
   assertCaptured "Oracle Client Installed"
 
   . $BUILDPACK_HOME/export
-  assertEquals $OCI_LIB_DIR "$BUILD_DIR/.oracle-build/instantclient"
-  assertEquals $OCI_INC_DIR "$BUILD_DIR/.oracle-build/instantclient/sdk/include"
-  assertTrue "OCI_INC_DIR missing" "[ -d '${OCI_INC_DIR}' ]"
+  assertEquals $OCI_LIB_DIR ".oracle-build/instantclient"
+  assertEquals $OCI_INC_DIR ".oracle-build/instantclient/sdk/include"
+  incDir="${BUILD_DIR}/${OCI_INC_DIR}"
+  assertTrue "OCI_INC_DIR missing" "[ -d '${incDir}' ]"
 }
