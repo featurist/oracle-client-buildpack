5649307fab77c6f9e047607dae70f1d1149e9129
artemave <artemave@gmail.com>
2017-11-28 09:53:18 +0000

    Export separate environment for runtime
    In an attempt to make compiled oracle module actually work.

---
+++ profile/oracle-driver-buildpack-runtime-env.sh
@@ -0,0 +1,3 @@
+export LD_LIBRARY_PATH="$HOME/.oracle-build/instantclient/${LD_LIBRARY_PATH:+:$LD_LIBRARY_PATH}"
+export OCI_LIB_DIR="$HOME/.oracle-build/instantclient"
+export OCI_INC_DIR="$HOME/.oracle-build/instantclient/sdk/include"
--- test/compile_test.sh
+++ test/compile_test.sh
@@ -8,8 +8,14 @@ testCompile() {
   assertCaptured "Oracle Client Installed"
 
   . $BUILDPACK_HOME/export
-  assertEquals $OCI_LIB_DIR ".oracle-build/instantclient"
-  assertEquals $OCI_INC_DIR ".oracle-build/instantclient/sdk/include"
-  incDir="${BUILD_DIR}/${OCI_INC_DIR}"
-  assertTrue "OCI_INC_DIR missing" "[ -d '${incDir}' ]"
+  assertEquals $LD_LIBRARY_PATH "$BUILD_DIR/.oracle-build/instantclient/"
+  assertEquals $OCI_LIB_DIR "$BUILD_DIR/.oracle-build/instantclient"
+  assertEquals $OCI_INC_DIR "$BUILD_DIR/.oracle-build/instantclient/sdk/include"
+  assertTrue "'\$OCI_LIB_DIR/libclntsh.so' missing" "[ -f '${OCI_LIB_DIR}/libclntsh.so' ]"
+  assertTrue "OCI_INC_DIR missing" "[ -d '${OCI_INC_DIR}' ]"
+
+  . $BUILD_DIR/.profile.d/oracle-driver-buildpack-runtime-env.sh
+  assertEquals $LD_LIBRARY_PATH "$HOME/.oracle-build/instantclient/:$BUILD_DIR/.oracle-build/instantclient/"
+  assertEquals $OCI_LIB_DIR "$HOME/.oracle-build/instantclient"
+  assertEquals $OCI_INC_DIR "$HOME/.oracle-build/instantclient/sdk/include"
 }
